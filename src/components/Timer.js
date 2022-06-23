cf83b1583c7d986a5ed6d12f03bedfa9fae71197
Ricardo Ramos <rf.ramos95@gmail.com>
2022-06-23 13:02:41 +0100

    Add responsive text btn and timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,10 +1,15 @@
 import { CountdownCircleTimer } from 'react-countdown-circle-timer';
 import { SettingContext } from './../context/SettingsContext';
 import { useContext } from 'react';
+import { useMediaQuery } from 'react-responsive';
 
 const Timer = ({ key = 1, time = 1, animate = false, children }) => {
 
   const { stopTimer } = useContext(SettingContext);
+  const is1300 = useMediaQuery({ query: `(max-width: 1300px)` });
+  const is1000 = useMediaQuery({ query: `(max-width: 1000px)` });
+  const is850 = useMediaQuery({ query: `(max-width: 850px)` });
+  const isMobile = useMediaQuery({ query: `(max-width: 450px)` });
 
   return (
     <CountdownCircleTimer
@@ -14,9 +19,15 @@ const Timer = ({ key = 1, time = 1, animate = false, children }) => {
       colors={[
           '#03B5AA'
         ]}
-      strokeWidth={15}
+      strokeWidth={ is850 ? 7 
+                   : is1000 ? 9
+                   : is1300 ? 11
+                   : 15 }
       trailColor="transparent"
-      size={450}
+      size={ is850 ? 240
+            : is1000 ? 300
+            : is1300 ? 370
+            : 450 }
       className="timer"
       onComplete={() => {
         stopTimer();
