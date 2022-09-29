b363d428abfd4579aac56113d318e30eea139985
Ricardo Ramos <rf.ramos95@gmail.com>
2022-09-29 16:42:52 +0100

    Add persistent session

--- src/auth/auth0-provider.js
+++ src/auth/auth0-provider.js
@@ -1,30 +1,32 @@
-import { Auth0Provider } from '@auth0/auth0-react';
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Auth0Provider } from '@auth0/auth0-react'
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Auth0ProviderWithHistory = ({ children }) => {
-  const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-  const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
-  const apiID = process.env.REACT_APP_AUTH0_API_ID;
+	const domain = process.env.REACT_APP_AUTH0_DOMAIN
+	const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID
+	const apiID = process.env.REACT_APP_AUTH0_API_ID
 
-  const history = useNavigate();
+	const history = useNavigate()
 
-  const onRedirectCallback = (appState) => {
-    history.push(appState?.returnTo || window.location.pathname);
-  };
+	const onRedirectCallback = appState => {
+		history.push(appState?.returnTo || window.location.pathname)
+	}
 
-  return (
-    <Auth0Provider
-        domain={domain}
-        clientId={clientId}
-        redirectUri={window.location.origin}
-        audience={apiID}
-        scope="openid profile email"
-        
-    >
-      {children}
-    </Auth0Provider>
-  );
-};
+	return (
+		<Auth0Provider
+			domain={domain}
+			clientId={clientId}
+			redirectUri={window.location.origin}
+			audience={apiID}
+			scope='openid profile email'
+			onRedirectCallback={onRedirectCallback}
+			useRefreshTokens
+			cacheLocation='localstorage'
+		>
+			{children}
+		</Auth0Provider>
+	)
+}
 
-export default Auth0ProviderWithHistory;
+export default Auth0ProviderWithHistory
